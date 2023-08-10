51056c51e3b2582dba8e3b1d7236e635f296aa8f
Crowdbotics <team@crowdbotics.com>
2023-08-10 00:51:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "inquinte-42761",
+  url: "https://inquinte-42761.botics.co",
+  api: "https://inquinte-42761.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
